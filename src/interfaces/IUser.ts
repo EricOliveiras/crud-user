a1b38c641deb244e3cf5ae0bb314559269d30b42
Eric Oliveira <oiericoi@hotmail.com>
2022-12-17 14:19:25 -0300

    refactor: add password property in user interface

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -2,10 +2,12 @@ export interface IUser {
   full_name: string;
   email: string;
   phone?: string;
+  password: string;
 }
 
 export interface IUserUpdate {
   full_name?: string;
   email?: string;
   phone?: string;
+  password: string;
 }
