d5e4f3d4ecda6518ec54b0724993fe12cd4fe613
Eric Oliveira <oiericoi@hotmail.com>
2022-12-20 11:43:05 -0300

    refactor: add express-async-errors import and modify confing import

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
+import 'express-async-errors';
 import { app } from './app';
-import { port } from './config/vars';
+import { port } from './config';
 
 app.listen(port, () => console.log(`Server is running in port ${port}! 🚀`));
