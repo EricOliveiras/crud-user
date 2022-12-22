1ddf8e17dd67db06ffca7091fe6494f25fd8ac3d
Eric Oliveira <oiericoi@hotmail.com>
2022-12-22 15:34:20 -0300

    feat: add login and logout methods, and add yours middlewares

--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -1,19 +1,38 @@
 import { Router } from 'express';
 import { UserController } from '../controller/user/UserController';
+import { authenticated } from '../middlewares/authenticateMiddleware';
+import { validateAuthenticate, validateCreateUser, validateDeleteUser, validateUpdateUser } from '../middlewares/userMiddleware';
 
 export const usersRouter = Router();
 
 usersRouter.
   post('/',
+    validateCreateUser,
     UserController.create
   );
 
 usersRouter.
-  patch('/update/:id',
+  post('/login',
+    validateAuthenticate,
+    UserController.login
+  );
+
+usersRouter.
+  post('/logout',
+    authenticated,
+    UserController.logout
+  );
+
+usersRouter.
+  put('/update',
+    authenticated,
+    validateUpdateUser,
     UserController.update
   );
 
 usersRouter.
-  delete('/delete/:id',
+  delete('/delete',
+    authenticated,
+    validateDeleteUser,
     UserController.delete
   );
