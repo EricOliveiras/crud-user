473f8cbe605bfea6f1e665eabdcf782222169491
Eric Oliveira <oiericoi@hotmail.com>
2022-12-22 15:32:22 -0300

    feat: add login and logout methods

--- src/controller/user/UserController.ts
+++ src/controller/user/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { IUser, IUserUpdate } from "../../interfaces/IUser";
 import { CreateUser, UpdateUser, DeleteUser } from "../../services/user";
+import { UserAuthenticate } from "../../services/user/UserAuthenticate";
 
 export const UserController = {
   async create(req: Request, res: Response) {
@@ -17,7 +18,7 @@ export const UserController = {
   },
 
   async update(req: Request, res: Response) {
-    const { id } = req.params;
+    const id = req.user?.id as string;
     const { full_name, email, phone, password }: IUserUpdate = req.body;
 
     await UpdateUser(id, {
@@ -31,10 +32,25 @@ export const UserController = {
   },
   
   async delete(req: Request, res: Response) {
-    const { id } = req.params;
+    const id = req.user?.id as string;
     
     await DeleteUser(id);
     
+    return res.sendStatus(200);
+  },
+
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const logged = await UserAuthenticate(email, password);
+    req.session.singin = true;
+
+    return res.status(200).json(logged);
+  },
+
+  async logout(req: Request, res: Response) {
+    req.session.singin = false;
+
     return res.sendStatus(200);
   }
 };
