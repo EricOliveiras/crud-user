f11d71249af6c43b314bc1de55bc52034e80719a
Eric Oliveira <oiericoi@hotmail.com>
2022-12-22 15:33:08 -0300

    feat: add express-session in app

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routers';
 import { ErrorException } from './middlewares/errorMiddleware';
+import session from 'express-session';
+import { secret } from './config';
 
 export const app = express();
+
+app.use(session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(express.json());
 app.use(cors());
